ae552fa6a6811715d1ad77d708be067367666011
Harshit <love.coolsachin@gmail.com>
2018-06-10 09:25:16 +0530

    3rd solution done

--- solutions.py
+++ solutions.py
@@ -37,26 +37,74 @@ def Question2(a):
 
 import collections
 
+parent=dict()
+rank=dict()
+
+def make_set(vertex):
+    parent[vertex]=vertex
+    rank[vertex]=0
+
+
+def find_set(vertex):
+    if parent[vertex] != vertex:
+        parent[vertex]= find_set(parent[vertex])
+    return parent[vertex]
+
+
+def union(vertex1,vertex2):
+    parent1=find_set(vertex1)
+    parent2=find_set(vertex2)
+    if parent1 != parent2:
+        if rank[parent1] > rank[parent2]:
+            parent[parent2]=parent1
+        elif rank[parent1] < rank[parent2]:
+            parent[parent1]=parent2
+        else:
+            parent[parent2]=parent1
+            rank[parent1] +=1
+
+
+def get_edges(adj):
+    edge_list=[]
+    for vertex,edges in adj.iteritems():
+        for edge in edges:
+            if vertex < edge[0]:
+                edge_list.append((edge[1],vertex,edge[0]))
+    return edge_list
+
+
 def Question3(G):
     """Given an undirected graph G, find the minimum spanning tree within G. A minimum spanning tree connects all vertices in a graph with the smallest possible total weight of edges. Your function should take in and return an adjacency list structured like this:"""
+    if not bool(G) :
+        return "Wrong Graph input"
+    for vertex in  G.keys():
+        make_set(vertex)
+    edges=get_edges(G)
+    MST=set()
+    edges.sort()
+    for weight,vertex1,vertex2 in edges:
+        if find_set(vertex1) != find_set(vertex2):
+            union(vertex1,vertex2)
+            MST.add((weight,vertex1,vertex2))
+    adj=collections.defaultdict(list)
+    for edge in MST:
+        weight=edge[0]
+        vertex1=edge[1]
+        vertex2=edge[2]
+        adj[vertex1].append((vertex2,weight))
+    return dict(adj)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+Graph1={'A': [('B', 2)],
+         'B': [('A', 2), ('C', 5)],
+          'C': [('B', 5)]}
+Graph2={}
+Graph3={'A':[('B',6),('C',8),('D',8)],
+        'B':[('C',7),('D',9)],
+        'C':[('D',4),('A',8),('B',7)],
+        'D':[('B',9),('C',4),('A',8)]}
+Graph4={'A': [('B', 2), ('C', 5)],
+        'B': [('A', 2), ('C', 4)],
+        'C': [('A', 5), ('B', 4)]}
 
 """
 print Question1("abdc","abd")
@@ -75,4 +123,13 @@ print Question2("")
 # LongestPalindromicSubstring "No String found"
 print Question2("abcdefgh")
 # LongestPalindromicSubstring "a"
+
+print Question3(Graph1)
+# {'A': [('B', 2)], 'C': [('B', 5)], 'B': [('C', 5), ('A', 2)]}
+print Question3(Graph2)
+# Wrong Graph input
+print Question3(Graph3)
+# {'A': [('B', 6)], 'C': [('D', 4)], 'B': [('C', 7)]}
+print Question3(Graph4)
+# {'A': [('B', 2)], 'B': [('C', 4)]}
 """
