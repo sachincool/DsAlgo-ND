480b28de5f8777878f5b659532d436c6eeab2c6b
Harshit <love.coolsachin@gmail.com>
2018-06-11 06:11:57 +0530

    final solutions

--- solutions.py
+++ solutions.py
@@ -106,15 +106,112 @@ Graph4={'A': [('B', 2), ('C', 5)],
         'B': [('A', 2), ('C', 4)],
         'C': [('A', 5), ('B', 4)]}
 
-"""
+class Element(object):
+    def __init__(self,data):
+        self.data=data
+        self.left=None
+        self.right=None
+
+class BST(object):
+    def __init__(self,root):
+        self.root=Element(root)
+    def insert(self,new_val):
+        self.insert_helper(self.root,new_val)
+
+    def insert_helper(self,current,new_val):
+        if current.data<new_val:
+            if current.right:
+                self.insert_helper(current.right,new_val)
+            else:
+                current.right=Element(new_val)
+
+        else:
+            if current.left:
+                self.insert_helper(current.left,new_val)
+            else:
+                current.left=Element(new_val)
+
+def lca(root,n1,n2):
+    if root is None:
+        return None
+    if root.data > max(n1,n2):
+        return lca(root.left,n1,n2)
+    elif root.data < min(n1,n2):
+        return lca(root.right,n1,n2)
+    else: return root.data
+
+def Question4(T,r,n1,n2):
+    """
+    Find the least common ancestor between two nodes on a binary search tree. The least common ancestor is the farthest node from the root that is an ancestor of both nodes. For example, the root is a common ancestor of all nodes on the tree, but if both nodes are descendents of the root's left child, then that left child might be the lowest common ancestor. You can assume that both nodes are in the tree, and the tree itself adheres to all BST properties. The function definition should look like question4(T, r, n1, n2), where T is the tree represented as a matrix, where the index of the list is equal to the integer stored in that node and a 1 represents a child node, r is a non-negative integer representing the root, and n1 and n2 are non-negative integers representing the two nodes in no particular order. For example, one test case might be
+    """
+    if not T:
+        return None
+    bst=BST(r)
+    for row in reversed(range(len(T))):
+        for node in range(len(T[row])):
+            if T[row][node] == 1:
+                bst.insert(node)
+    return lca(bst.root,n1,n2)
+
+T=[[0, 1, 0, 0, 0],[0, 0, 0, 0, 0],[0, 0, 0, 0, 0],[1, 0, 0, 0, 1],[0, 0, 0, 0, 0]]
+T2=[
+[0,0,0,0],
+[1,0,0,0],
+[0,1,0,1],
+[0,0,0,0]]
+T3=[]
+T4=[[0, 0, 0, 0, 0],
+    [1, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 1],
+    [0, 0, 0, 0, 0]]
+
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+class LinkedList(object):
+    def __init__(self,head=None):
+        self.head=head
+    def append(self,new_element):
+        current=self.head
+        if self.head:
+            while current.next:
+                current=current.next
+            current.next=Node(new_element)
+        else: self.head=Node(new_element)
+
+def Question5(ll,m):
+    if type(m) is not int or m <=0:
+        return "wrong value of m"
+    pointer1=ll
+    pointer2=ll
+    count=1
+    while count < m:
+        pointer1=pointer1.next
+        count+=1
+    while pointer1.next:
+        pointer1=pointer1.next
+        pointer2=pointer2.next
+    return pointer2.data
+
+ll= LinkedList(Node("0"))
+ll.append("1")
+ll.append("2")
+ll.append("3")
+ll.append("4")
+ll.append("5")
+ll.append("6")
+ll.append(7)
+
+# Driver code
 print Question1("abdc","abd")
 # True
 print Question1(""," ad")
 # False
 print Question1(""," ")
 # False
-
-
+print "============================="
 print Question2("abababa")
 # LongestPalindromicSubstring "abababa"
 print Question2("abacdfgdcaba")
@@ -124,6 +221,7 @@ print Question2("")
 print Question2("abcdefgh")
 # LongestPalindromicSubstring "a"
 
+print "============================="
 print Question3(Graph1)
 # {'A': [('B', 2)], 'C': [('B', 5)], 'B': [('C', 5), ('A', 2)]}
 print Question3(Graph2)
@@ -132,4 +230,18 @@ print Question3(Graph3)
 # {'A': [('B', 6)], 'C': [('D', 4)], 'B': [('C', 7)]}
 print Question3(Graph4)
 # {'A': [('B', 2)], 'B': [('C', 4)]}
-"""
+
+print "============================="
+print Question4(T,3,1,4)
+# 3
+print Question4(T2,2,1,0)
+# 1
+print Question4(T3,1,1,1)
+# None
+print Question4(T4,3,1,2)
+# 1
+
+print "============================="
+print Question5(ll.head,2) #print 6
+print Question5(ll.head,-1) # print "wrong value of m"
+print Question5(ll.head,8) # print 0
